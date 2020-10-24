850f77d5202e0b27dbcf127ecb3931eebb2f0e58
Szymon Nowak <szymon.nowak@allegro.pl>
2020-10-24 10:05:27 +0200

    initial commit

---
+++ src/main/java/com/sda/javapoz22projekt/Javapoz22ProjektApplication.java
@@ -0,0 +1,14 @@
+package com.sda.javapoz22projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Javapoz22ProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javapoz22ProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/javapoz22projekt/Javapoz22ProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.javapoz22projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javapoz22ProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
