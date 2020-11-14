3b35fcc5f6cc57121b206a039e538986183d8ae8
Szymon Nowak <szymon.nowak@allegro.pl>
2020-11-14 11:45:11 +0100

    find all offers

--- src/main/java/com/sda/javapoz22projekt/controller/OfferController.java
+++ src/main/java/com/sda/javapoz22projekt/controller/OfferController.java
@@ -15,10 +15,17 @@ public class OfferController {
         this.offerService = offerService;
     }
 
-    @GetMapping("/offer/{id}")
+    @GetMapping("/offers/{id}")
     public ModelAndView offerById(@PathVariable("id") Long id) {
         ModelAndView modelAndView = new ModelAndView("offer");
         modelAndView.addObject("offer", offerService.getById(id));
+        return modelAndView;
+    }
+
+    @GetMapping("/offers")
+    public ModelAndView findAll() {
+        ModelAndView modelAndView = new ModelAndView("offers");
+        modelAndView.addObject("offers", offerService.findAll());
         return modelAndView;
     }
 }
